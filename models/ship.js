97944a90bc3504b407b7aef16d80db4695aaf9a3
Drew Robinson <drewbie0307@hotmail.com>
2014-02-27 22:37:54 -0600

    Game logic checks to see if x, y value is true and if false, picks new numbers and starts the process over again.

--- models/ship.js
+++ models/ship.js
@@ -24,6 +24,8 @@
 
     this.availablePlayerSpace = this.createAvailableSpaces();
 
+    this.availableComputerSpace = this.createAvailableSpaces();
+
     this.undragPlayerShips = function() {
       for (var i = 0; i < this.playerShips.length; i++) {
         this.playerShips[i].undrag();
@@ -40,17 +42,17 @@
 
     }
 
-    this.overlappingShips = function() {
-      for (var k = 0; k < this.playerShips.length; k++) {
-        for (var i = 0; i < this.playerShips.length; i++) {
-          if (Snap.path.isBBoxIntersect(this.playerShips[i].getBBox(), this.playerShips[k].getBBox()) === true) {
-            if (i !== k && i < k) {
-              console.log("I'm overlapping!");
-            }
-          }    
-        }
-      }
-    }
+    // this.overlappingShips = function() {
+    //   for (var k = 0; k < this.playerShips.length; k++) {
+    //     for (var i = 0; i < this.playerShips.length; i++) {
+    //       if (Snap.path.isBBoxIntersect(this.playerShips[i].getBBox(), this.playerShips[k].getBBox()) === true) {
+    //         if (i !== k && i < k) {
+    //           console.log("I'm overlapping!");
+    //         }
+    //       }    
+    //     }
+    //   }
+    // }
 
     this.placeShip = function(x, y, width, type) {
       var ship;
@@ -68,28 +70,19 @@
 
       var grid = createGrid(svgId);
       
-      // var position = this.availablePlayerSpace[util.randInt(0, 255)];
-      // var x = position[0];
-      // var y = position[1];
-      // var blocks = width / 25;
-      
       if (type === 'player') {
-        
-        // Check if space is available
-
         if (this.availablePlayerSpace[x + ',' + y] === false) {
           this.availablePlayerSpace[x + ',' + y] = true; 
           
-          var shipGroup = grid.canvas.group();
+          var playerShipGroup = grid.canvas.group();
           
           for (var i = 0; i < width; i++){
-            shipGroup.add(grid.canvas.rect((x * 25), (y * 25), 25, 25));
-          
+            playerShipGroup.add(grid.canvas.rect((x * 25), (y * 25), 25, 25));   
             x++;
           }
-          shipGroup.drag();
+          playerShipGroup.drag();
         }
-
+        this.playerShips.push(playerShipGroup);
         // shipGroup.drag(
         //   function (dx, dy, x, y, e) {
         //     var xSnap = Snap.snapTo(25, grid.origin.x + dx, 100000000);
@@ -105,23 +98,30 @@
         //     grid.origin.y = e.toElement.y.baseVal.value;
         //   }
         // );
-        this.playerShips.push(ship);
-      }
-      else {
-        // computerShipsObject[x,y] = true;
-        x = x * 25;
-        y = y * 25;
-        var x1 = x + (width * 25);
-        var y1 = y + 25;
-        ship = grid.canvas.path('M' + x + ',' + y + 'L' + x1 + ',' + y + 'L' + x1 + ',' + y1 + 'L' + x + ',' + y1 + ',z');
-        ship.attr('fill', 'black');
-        this.computerShips.push(ship);
+        
+      } else {
+        if (this.availableComputerSpace[x + ',' + y] === false) {
+          this.availableComputerSpace[x + ',' + y] = true;
+
+          var computerShipGroup = grid.canvas.group();
+
+          for (var i = 0; i < width; i++) {
+            computerShipGroup.add(grid.canvas.rect((x * 25), (y * 25), 25, 25));
+            x++;
+          }
+
+          this.computerShips.push(computerShipGroup);
+        } else {
+          console.log("I'm overlapping!");
+          
+          x = util.randInt(0, 15);
+          y = util.randInt(0, 15);
+          this.placeShip(x, y, width, 25);
+        }
       }
       return {
         grid: grid,
         ship: ship,
-        // availablePlayerSpace: playerShipsObject,
-        //availableComputerSpace: computerShipsObject
       }
     };
 
--- presenters/ship-presenter.js
+++ presenters/ship-presenter.js
@@ -7,16 +7,25 @@ $(document).ready(function(){
     ships.placeShip(4, 8, 4, 'player');
     ships.placeShip(9, 9, 5, 'player');
 
-    ships.placeShip(3, 3, 2, 'cpu');
-    ships.placeShip(3, 4, 3, 'cpu');
-    ships.placeShip(6, 5, 3, 'cpu');
-    ships.placeShip(4, 8, 4, 'cpu');
-    ships.placeShip(10, 8, 5, 'cpu');
+    var values = [];
+    for (var i = 0; i < 5; i++) {
+      var x = util.randInt(0, 15);
+      var y = util.randInt(0, 15);
+      values.push([x, y]);
+    }
+
+
+    ships.placeShip(values[0][0], values[0][1], 2, 'cpu');
+    ships.placeShip(values[1][0], values[1][1], 3, 'cpu');
+    ships.placeShip(values[2][0], values[2][1], 3, 'cpu');
+    ships.placeShip(values[3][0], values[3][1], 4, 'cpu');
+    ships.placeShip(values[4][0], values[4][1], 5, 'cpu');
 
 
   $('#button').click(function(e){
     e.preventDefault();
     ships.undragPlayerShips();
+    console.log(ships.availableComputerSpace);
   })
 })
     
