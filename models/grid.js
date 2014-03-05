492ed8428d88c1ec956491aec2cd328ea78c5775
Drew Robinson <drewbie0307@hotmail.com>
2014-03-05 13:11:07 -0600

    Broke the SnapTo grid again but fixed it as well. Also only placing one player grid and one computer grid

--- models/grid.js
+++ models/grid.js
@@ -1,36 +1,44 @@
-(function() {
+var aGrid = (function(){
+  var instance;
+  
+  function init() {
+    var playerGrid = Snap('#svg1');
+    var computerGrid = Snap('#svg2');
+    var gridSize = 25;
+    var orig = {
+      x: 0,
+      y: 0
+    };
+  
+    var boxes = [];
+    for (var i = 0; i <= 400; i += 25) {
+      for (var k = 0; k <= 400; k += 25) {
+        var box = playerGrid.path('M'+ i + ',' + k + 'L' + (i+25) + ',' + k + 'L' + (i+25) + ',' + (k+25) + 'L' + i + ',' + (k+25) + ',z');
+        var box2 = computerGrid.path('M'+ i + ',' + k + 'L' + (i+25) + ',' + k + 'L' + (i+25) + ',' + (k+25) + 'L' + i + ',' + (k+25) + ',z');
+        attr = {
+          fill: 'none',
+          stroke: 'black',
+          strokeWidth: 2
+        };
+        box.attr(attr);
+        box2.attr(attr);
+        boxes.push(box);
+           
+      }   
+    }
 
-  var Grid = function() {
-
-    this.createGrid = function(canvasId){
-      var s = Snap(canvasId);
-      var gridSize = 25;
-      var orig = {
-        x: 0,
-        y: 0
-      };
-
-      var boxes = [];
-      for (var i = 0; i <= 400; i += 25) {
-        for (var k = 0; k <= 400; k += 25) {
-          var box = s.path('M'+ i + ',' + k + 'L' + (i+25) + ',' + k + 'L' + (i+25) + ',' + (k+25) + 'L' + i + ',' + (k+25) + ',z');
-          box.attr({
-            fill: 'none',
-            stroke: 'black',
-            strokeWidth: 2
-          })
-          boxes.push(box);
-          
-        }
-        
-      }
-      return {
-        canvas: s,
-        gridSize: gridSize,
-        origin: orig,
-        boxes: boxes
-      };
+    return {
+      playerGrid: playerGrid,
+      computerGrid : computerGrid
     };
   };
-  window.grids = new Grid();
-})();
+
+  return {
+    getInstance: function() {
+      if (!instance) {
+        instance = init();
+      }
+      return instance;
+    }
+  };
+})()
--- models/ship.js
+++ models/ship.js
@@ -6,15 +6,6 @@
 
     this.computerShips = [];
 
-    this.placeGrids = function(canvasId) {
-      var grid = grids.createGrid(canvasId);
-      return grid;
-    }
-
-    this.playerGrid = this.placeGrids('#svg1');
-
-    this.computerGrid = this.placeGrids('#svg2');
-
     this.createAvailableSpaces = function() {
       var gridArray = [];
 
@@ -52,76 +43,81 @@
 
     this.placePlayerShip = function(x, y, width) {
       var ship;
+      var orig = {
+        x: 0,
+        y: 0
+      };
 
-      var grid = this.playerGrid.canvas;
-    
-        if (this.availablePlayerSpace[x + ',' + y] === false) {
-          this.availablePlayerSpace[x + ',' + y] = true; 
-          
-          var playerShipGroup = grid.group();
-          
-          for (var i = 0; i < width; i++){
-            playerShipGroup.add(grid.rect((x * 25), (y * 25), 25, 25));   
-            x++;
-          }
-          playerShipGroup.drag(
-            function (dx, dy, x, y, e) {
-              var xSnap = Snap.snapTo(25, grid.origin.x + dx, 100000000);
-              var ySnap = Snap.snapTo(25, grid.origin.y + dy, 100000000);
+      var grid = aGrid.getInstance().playerGrid;
 
-              x = 0;
-              var max = 400;
-              for (var i = 0; i < width; i += 1) {                
-                  this[i].attr({
-                    x: xSnap,
-                    y: ySnap
-                  });
-              }
-               xSnap += 25;              
-              }  
-            },
-
-            function (x, y, e){
-              grid.origin.x = e.toElement.x.baseVal.value;
-              grid.origin.y = e.toElement.y.baseVal.value;
-            }
-          );
-        }
-        this.playerShips.push(playerShipGroup);
+      if (this.availablePlayerSpace[x + ',' + y] === false) {
+        this.availablePlayerSpace[x + ',' + y] = true; 
         
-        return {
-          playGrid: grid,
-          ship: ship
-        }
+        var playerShipGroup = grid.group();
+        
+        for (var i = 0; i < width; i++){
+          playerShipGroup.add(grid.rect((x * 25), (y * 25), 25, 25));   
+          x++;
+        }    
       }
-        
-      this.placeComputerShip = function(x, y, width) {
-        var ship;
-        var grid = this.computerGrid.canvas;
 
-        if (this.availableComputerSpace[x + ',' + y] === false && this.availableComputerSpace[(x + width) + ',' + y] === false) {
-          this.availableComputerSpace[x + ',' + y] = true;
-          this.availableComputerSpace[(x + width) + ',' + y] = true;
-          var computerShipGroup = grid.group();
+      playerShipGroup.drag(
+        function (dx, dy, x, y) {
+          var xSnap = Snap.snapTo(25, orig.x + dx, 100000000);
+          var ySnap = Snap.snapTo(25, orig.y + dy, 100000000);
 
-          for (var i = 0; i < width; i++) {
-            computerShipGroup.add(grid.rect((x * 25), (y * 25), 25, 25));
-            x++;
-          }
+          for (var i = 0; i < width; i += 1) {               
+            this[i].attr({
+              x: xSnap,
+              y: ySnap
+            });
+            xSnap += 25;
+          }                
+        },
 
-          this.computerShips.push(computerShipGroup);
-        } else {
-          console.log("I'm overlapping!");
-          x = util.randInt(0, 15);
-          y = util.randInt(0, 15);
-          this.placeComputerShip(x, y, width, 25);
+        function (x, y, e){
+          orig.x = e.toElement.x.baseVal.value;
+          orig.y = e.toElement.y.baseVal.value;
         }
-          return {
-          compGrid: grid,
-          ship: ship
+      );
+
+      this.playerShips.push(playerShipGroup);
+      
+      return {
+        playGrid: grid,
+        ship: ship
+      }
+    }
+    
+ 
+    this.placeComputerShip = function(x, y, width) {
+      var ship;
+      var g = aGrid.getInstance();
+      var grid = g.computerGrid;
+
+      if (this.availableComputerSpace[x + ',' + y] === false && this.availableComputerSpace[(x + width) + ',' + y] === false) {
+        this.availableComputerSpace[x + ',' + y] = true;
+        this.availableComputerSpace[(x + width) + ',' + y] = true;
+        var computerShipGroup = grid.group();
+
+        for (var i = 0; i < width; i++) {
+          computerShipGroup.add(grid.rect((x * 25), (y * 25), 25, 25));
+          x++;
         }
-      } 
-    };
+
+        this.computerShips.push(computerShipGroup);
+      } else {
+        console.log("I'm overlapping!");
+        x = util.randInt(0, 15);
+        y = util.randInt(0, 15);
+        this.placeComputerShip(x, y, width, 25);
+      }
+        return {
+        compGrid: grid,
+        ship: ship
+      }
+    } 
+  };
 
   window.ships = new Ship();
 })();
